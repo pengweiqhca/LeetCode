b085b8089f9af6c3a493ab5d306c0d1e2cb45b9d
彭伟 <pengweiqhca@sina.com>
2020-03-20 23:25:20 +0800

    优化代码

--- LeetCode/P1/LeetCode174.cs
+++ LeetCode/P1/LeetCode174.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LeetCode.P1
@@ -9,26 +10,16 @@ namespace LeetCode.P1
         {
             int xLength = dungeon.Length, yLength = dungeon.Max(m => m.Length);
 
-            var map = new int[xLength, yLength];
-            for (var x = 0; x < dungeon.Length; x++)
-            {
-                var array = dungeon[x];
-                for (var y = 0; y < yLength; y++)
-                {
-                    map[x, y] = y >= array.Length ? 0 : array[y];
-                }
-            }
-
-            return CalculateMinimumHp(map, new int[xLength, yLength], xLength, yLength, 0, 0);
+            return CalculateMinimumHp(dungeon, new int[xLength, yLength], xLength, yLength, 0, 0);
         }
 
-        private static int CalculateMinimumHp(int[,] dungeon, int[,] minHp, int xLength, int yLength, int x, int y) =>
+        private static int CalculateMinimumHp(IReadOnlyList<IReadOnlyList<int>> dungeon, int[,] minHp, int xLength, int yLength, int x, int y) =>
             minHp[x, y] > 0
                 ? minHp[x, y]
                 : minHp[x, y] = Math.Max(1, (x == xLength - 1
                     ? y == yLength - 1 ? 1 : CalculateMinimumHp(dungeon, minHp, xLength, yLength, x, y + 1)
                     : y == yLength - 1
                         ? CalculateMinimumHp(dungeon, minHp, xLength, yLength, x + 1, y)
-                        : Math.Min(CalculateMinimumHp(dungeon, minHp, xLength, yLength, x, y + 1), CalculateMinimumHp(dungeon, minHp, xLength, yLength, x + 1, y))) - dungeon[x, y]);
+                        : Math.Min(CalculateMinimumHp(dungeon, minHp, xLength, yLength, x, y + 1), CalculateMinimumHp(dungeon, minHp, xLength, yLength, x + 1, y))) - dungeon[x][y]);
     }
 }
